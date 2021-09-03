956a205eca5a43e4da0af5b28d36b49dd91ac2d5
LeonardoLegramante <leonardo.legramante@kron.digital>
2021-09-03 15:25:28 -0300

    .

---
+++ Node/05-ThirdChapter-Continuation-RentalX/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+
+import { router } from './routes'
+import swaggerFiler from './swagger.json'
+
+import './database'
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/api-docks', swaggerUi.serve, swaggerUi.setup(swaggerFiler))
+
+app.use(router)
+
+app.listen(3333, () => console.log('Server is running!'))
